db71889a16fd57b6225f796b306d66664091b1b9
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-13 13:56:10 +0200

    controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action: find_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -55,4 +56,52 @@ class TasksController < ApplicationController
     # new Task show up immediately without having to go through checks.
     params.require(:task).permit(:title, :details, :completed)
   end
+
+  def find_task
+  end
 end
+
+# @task = Task.find(params[:id]) is repeated loads.
+# REFACTOR
+
+# class TasksController < ApplicationController
+#   before_action: find_task, only: [:show, :edit, :update, :destroy]
+
+#   def index
+#     @tasks = Task.all
+#   end
+
+#   def show; end
+
+#   def new
+#     @task = Task.new
+#   end
+
+#   def create
+#     @task = Task.new(task_params)
+#     @task.save
+#     redirect_to tasks_path(@task)
+#   end
+
+#   def edit; end
+
+#   def update
+#     @task.update(task_params)
+#     redirect_to tasks_path(@task)
+#   end
+
+#   def destroy
+#     @task.destroy
+#     redirect_to tasks_path
+#   end
+
+#   private
+
+#   def task_params
+#     params.require(:task).permit(:title, :details, :completed)
+#   end
+
+#   def find_task
+#     @task = Task.find(params[:id])
+#   end
+# end
