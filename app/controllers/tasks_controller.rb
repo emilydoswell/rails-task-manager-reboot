16f054ab79790ee607fa3dccf9238308c9638353
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-13 13:04:34 +0200

    controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,8 +22,9 @@ class TasksController < ApplicationController
     # Also today it doesn't matter if you do Task.new task.save or Task.create
     # Tomorrow we'll do validations, if save? ... do this else re render the form.
 
-    redirect_to task_path
+    redirect_to task_path(@task)
     # The prefix to the show page is task, so we redirect to the task_path which is the show page of all the tasks.
+    # We need (@task) because the show page has a :id in the url which is dynamic, so you pass it the instance of @task for the id.
   end
 
   def edit
