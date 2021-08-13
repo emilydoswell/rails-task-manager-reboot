df36f8d34daa91b31a7ea4dbe3845d90c0e7df10
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-13 11:01:33 +0200

    CRUD actions in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb "url", to: "controller#action"
-  get "/task", to: "tasks#task"
+  get "/tasks", to: "tasks#index"
+
+  # CRUD
+  # CREATE, READ, UPDATE, DELETE
+
+  # Read all the tasks
+  get "/tasks", to: "tasks#index"
+
+  # Read one task
+  get "/tasks/:id", to: "tasks#show"
+
+  # Create a task
+  get "/tasks/new", to: "tasks#new" # Display the form
+  post "/tasks", to: "tasks#create"
+
+  # Update a task
+  get "/tasks/:id/edit", to: "tasks#edit"
+  patch "/tasks/:id", to: "tasks#update"
+
+  # Delete a task
+  delete "/tasks/:id", to: "tasks#destroy"
 end
