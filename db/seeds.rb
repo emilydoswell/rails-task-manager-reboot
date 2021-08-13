5c6718374fd50124f1c45770717217d19ea2fd39
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-13 11:56:09 +0200

    index crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do 
+  Task.create(
+    title: Faker::House.furniture,
+    details: "clean",
+    completed: "false"
+  )
+end
+
+# gem 'Faker'
+# bundle install
+
+# Then rails db:seed
