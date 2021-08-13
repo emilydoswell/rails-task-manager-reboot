e633d80d354246c8682be6ad40e45a253467f9aa
emily doswell <emilydoswell@yahoo.co.uk>
2021-08-13 13:00:44 +0200

    index crud

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,13 @@ class TasksController < ApplicationController
   end
 
   def create
+    #  raise, put a raise here then go to http://localhost:3000/tasks/new, fill in the form then play around with params[:task]
+    # @task = Task.new(params[:task])
+    # task_params is an updated version of params[:task] which is more secure.
+    @task = Task.new(task_params)
+    @task.save
+    # Also today it doesn't matter if you do Task.new task.save or Task.create
+    # Tomorrow we'll do validations, if save? ... do this else re render the form.
   end
 
   def edit
@@ -23,5 +30,14 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    # Strong params - This says, only look at these fields, don't look at anything else.. e.g if a hacker came along and edited the html in
+    # their browser to add another field to the form (permitted for example), whereby they could check the box permitted, and make their
+    # new Task show up immediately without having to go through checks.
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
